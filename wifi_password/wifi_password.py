60d0318a4503b01fa7d46d7a94012d9975ced941
pelonchasva <andres.itson@gmail.com>
2021-02-04 02:43:29 -0700

    Moved some functions to utils.py, also added a new argument to print the list of saved networks in the os

--- wifi_password/wifi_password.py
+++ wifi_password/wifi_password.py
@@ -4,43 +4,41 @@
 #
 import pathlib
 import sys
-import subprocess
 import argparse
 from shutil import which
 import re
 import os
-import qrcode
+
+import utils
+import constants
 
 __version__ = "1.0.3"
-
-def run_command(command):
-    output, _ = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True).communicate()
-    return output.decode("utf-8")
-
 
 def print_error(text):
     print(f"ERROR: {text}")
     sys.exit(1)
 
-
 def get_ssid():
-    if sys.platform == "darwin":
+    platform = utils.get_platform()
+
+    if platform == constants.MAC:
         airport = pathlib.Path("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport")
         if not airport.is_file():
             print_error(f"Can't find 'airport' command at {airport}")
 
-        ssid = run_command(f"{airport} -I | awk '/ SSID/ {{print substr($0, index($0, $2))}}'")
+        ssid = utils.run_command(f"{airport} -I | awk '/ SSID/ {{print substr($0, index($0, $2))}}'")
         ssid = ssid.replace("\n", "")
 
-    elif sys.platform == "linux":
+    elif platform == constants.LINUX:
         if which("iwgetid") is None:
             print_error("Can't find the 'iwgetid' command\nPlease ensure wireless-tools is installed on your machine.")
         
-        ssid = run_command("iwgetid -r")
+        ssid = utils.run_command("iwgetid -r")
         ssid = ssid.replace("\n", "")
 
-    elif sys.platform == "win32":
-        ssid = run_command("netsh wlan show interfaces | findstr SSID").replace("\r", "")
+    elif platform == constants.WINDOWS:
+        ssid = utils.run_command("netsh wlan show interfaces | findstr SSID").replace("\r", "")
+
         if ssid == "":
             print_error("SSID was not found")
         
@@ -48,73 +46,38 @@ def get_ssid():
 
     return ssid
 
-
-def get_password(ssid):
-    if ssid == "":
-        print_error("SSID is not defined")
-
-    if sys.platform == "darwin":
-        password = run_command(f"security find-generic-password -l \"{ssid}\" -D 'AirPort network password' -w")
-        password = password.replace("\n", "")
-
-    elif sys.platform == "linux":
-        # Check if the user is running with super user privilages
-        if os.geteuid() != 0:
-            print_error(f"You need to run '{sys.argv[0]}' as root")
-
-        password = run_command(f"cat /etc/NetworkManager/system-connections/{ssid}.nmconnection | grep psk=")
-        password = password.replace("\n", "")
-        password = password[4:]
-
-    elif sys.platform == "win32":
-        password = run_command(f"netsh wlan show profile name=\"{ssid}\" key=clear | findstr Key").replace("\r", "")
-        password = re.findall(r"Key Content\s+:\s(.*)", password)[0]
-
-    if password == "":
-        print_error("Could not find password")
-
-    return password
-
-
-def generate_qr_code(ssid, password, image=False):
-    # Source: https://git.io/JtLIv
-    text = f"WIFI:T:WPA;S:{ssid};P:{password};;"
-
-    qr = qrcode.QRCode(version=1,
-                       error_correction=qrcode.constants.ERROR_CORRECT_L,
-                       box_size=10,
-                       border=4)
-    qr.add_data(text)
-
-    if image:
-        file_name = ssid.replace(" ", "_") + ".png"
-        img = qr.make_image()
-        img.save(file_name)
-        print(f"QR code has been saved to {file_name}")
-    else:
-        qr.make()
-        qr.print_tty()
-
-
 def main():
     parser = argparse.ArgumentParser(usage='%(prog)s [options]')
     parser.add_argument('--qrcode', "-q", action="store_true", default=False, help="Generate a QR code")
     parser.add_argument('--image', "-i", action="store_true", default=False, help="Create the QR code as image instead of showing it on the terminal (must be used along with --qrcode)")
-    parser.add_argument('--ssid', "-s", default=get_ssid(), help="Specify a SSID that you have previously connected to")
+    parser.add_argument('--ssid', "-s", nargs='?', type=str, help="Specify a SSID that you have previously connected to")
+    parser.add_argument('--list', "-l", action="store_true", default=False, help="Lists all stored network SSID")
     parser.add_argument('--version', action="store_true", help="Show version number")
     args = parser.parse_args()
 
     if args.version:
         print(__version__)
 
-    password = get_password(args.ssid)
+    wifi_list = {}
+
+    if args.ssid:
+        ssid = args.ssid.split(',')
+        wifi_list = utils.generate_wifi_list(ssid)
+        utils.print_wifi_list(wifi_list)
+
+    if args.list:
+        ssid = utils.get_ssid_list()
+        wifi_list = utils.generate_wifi_list(ssid)
+        utils.print_wifi_list(wifi_list)
+        return
 
     if args.qrcode:
         args.no_password = True
-        generate_qr_code(args.ssid, password, image=args.image)
-        return
 
-    print(password)
+        for key, value in wifi_list.items():
+            utils.generate_qr_code(key, value, image=args.image)
+        
+        return
 
 if __name__ == "__main__":
     main()

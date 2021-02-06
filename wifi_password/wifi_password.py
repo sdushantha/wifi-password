cd1282500cf99a96a863d40da584b0f10dc000e3
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2021-02-06 22:21:40 +0900

    Update korean patch

--- wifi_password/wifi_password.py
+++ wifi_password/wifi_password.py
@@ -10,16 +10,15 @@ from shutil import which
 import re
 import os
 import qrcode
-
+import locale
 __version__ = "1.0.7"
 
 def run_command(command):
     output, _ = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True).communicate()
-    return output.decode("utf-8").rstrip('\r\n')
-
+    return output.decode("euc-kr")
 
 def print_error(text):
-    print(f"ERROR: {text}", file=sys.stderr)
+    print(f"ERROR: {text}")
     sys.exit(1)
 
 
@@ -30,15 +29,17 @@ def get_ssid():
             print_error(f"Can't find 'airport' command at {airport}")
 
         ssid = run_command(f"{airport} -I | awk '/ SSID/ {{print substr($0, index($0, $2))}}'")
+        ssid = ssid.replace("\n", "")
 
     elif sys.platform == "linux":
         if which("nmcli") is None:
             print_error("Network Manager is required to run this program on Linux.")
 
         ssid = run_command("nmcli -t -f active,ssid dev wifi | egrep '^yes:' | sed 's/^yes://'")
+        ssid = ssid.replace("\n", "")
 
     elif sys.platform == "win32":
-        ssid = run_command("netsh wlan show interfaces | findstr SSID")
+        ssid = run_command("netsh wlan show interfaces | findstr SSID").replace("\r", "")
         if ssid == "":
             print_error("SSID was not found")
 
@@ -53,6 +54,7 @@ def get_password(ssid):
 
     if sys.platform == "darwin":
         password = run_command(f"security find-generic-password -l \"{ssid}\" -D 'AirPort network password' -w")
+        password = password.replace("\n", "")
 
     elif sys.platform == "linux":
         # Check if the user is running with super user privilages
@@ -61,10 +63,16 @@ def get_password(ssid):
         else:
             password = run_command(f"nmcli -s -g 802-11-wireless-security.psk connection show '{ssid}'")
 
-    elif sys.platform == "win32":
-        password = run_command(f"netsh wlan show profile name=\"{ssid}\" key=clear | findstr Key")
-        password = re.findall(r"Key Content\s+:\s(.*)", password)[0]
+        password = password.replace("\n", "")
 
+    elif sys.platform == "win32":
+        lang, _ = locale.getdefaultlocale()
+        key = "키" if lang == "ko_KR" else "Key"
+        password = run_command(f"netsh wlan show profiles name=\"{ssid}\" key=clear | findstr {key}").replace("\r", "")
+        try:
+            password = re.findall(r"키 콘텐츠\s+:\s(.*)", password)[0]
+        except IndexError:
+            password = ""
     if password == "":
         print_error("Could not find password")
 
@@ -75,7 +83,7 @@ def generate_qr_code(ssid, password, image=False):
     # Source: https://git.io/JtLIv
     text = f"WIFI:T:WPA;S:{ssid};P:{password};;"
 
-    qr = qrcode.QRCode(version=1,
+    qr = qrcode.QRCode(version=2,
                        error_correction=qrcode.constants.ERROR_CORRECT_L,
                        box_size=10,
                        border=4)
@@ -86,6 +94,8 @@ def generate_qr_code(ssid, password, image=False):
         img = qr.make_image()
         img.save(file_name)
         print(f"QR code has been saved to {file_name}")
+        os.system(file_name)
+        print(img)
     else:
         qr.make()
         qr.print_tty()

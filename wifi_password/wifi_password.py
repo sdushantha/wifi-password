770b3e5ab5b0324851089545cb6bd00e3ab2c718
Célian <celiandu61@cartooncraft.fr>
2021-02-12 17:00:23 +0100

    Revert "Fix merge conflict"
    This reverts commit 87069af94eab3513de2332d1afe0f068ec3283bc, reversing
    changes made to 7436bcd609d6712ac7c0475e2158e2c58ca2a52a.

--- wifi_password/wifi_password.py
+++ wifi_password/wifi_password.py
@@ -10,19 +10,18 @@ from shutil import which
 import re
 import os
 import qrcode
-import colorama
 
-__version__ = "1.0.9"
+__version__ = "1.0.6"
 
 def run_command(command):
     env = os.environ.copy()
     env["LANG"] = "C" 
     output, _ = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True, env=env).communicate()
-    return output.decode("utf-8").rstrip('\r\n')
+    return output.decode("utf-8")
 
 
 def print_error(text):
-    print(f"ERROR: {text}", file=sys.stderr)
+    print(f"ERROR: {text}")
     sys.exit(1)
 
 
@@ -33,18 +32,20 @@ def get_ssid():
             print_error(f"Can't find 'airport' command at {airport}")
 
         ssid = run_command(f"{airport} -I | awk '/ SSID/ {{print substr($0, index($0, $2))}}'")
+        ssid = ssid.replace("\n", "")
 
     elif sys.platform == "linux":
         if which("nmcli") is None:
             print_error("Network Manager is required to run this program on Linux.")
 
-        ssid = run_command("nmcli -t -f active,ssid dev wifi | egrep '^yes:' | sed 's/^yes://'")
+        ssid = run_command("nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\: -f2")
+        ssid = ssid.replace("\n", "")
 
     elif sys.platform == "win32":
-        ssid = run_command("netsh wlan show interfaces | findstr SSID")
+        ssid = run_command("netsh wlan show interfaces | findstr SSID").replace("\r", "")
         if ssid == "":
             print_error("SSID was not found")
-
+        
         ssid = re.findall(r"[^B]SSID\s+:\s(.*)", ssid)[0]
 
     return ssid
@@ -56,16 +57,18 @@ def get_password(ssid):
 
     if sys.platform == "darwin":
         password = run_command(f"security find-generic-password -l \"{ssid}\" -D 'AirPort network password' -w")
+        password = password.replace("\n", "")
 
     elif sys.platform == "linux":
         # Check if the user is running with super user privilages
         if os.geteuid() != 0:
-            password = run_command(f"sudo nmcli -s -g 802-11-wireless-security.psk connection show '{ssid}'")
-        else:
-            password = run_command(f"nmcli -s -g 802-11-wireless-security.psk connection show '{ssid}'")
+            print_error(f"You need to run '{sys.argv[0]}' as root")
+
+        password = run_command(f"nmcli -s -g 802-11-wireless-security.psk connection show '{ssid}'")
+        password = password.replace("\n", "")
 
     elif sys.platform == "win32":
-        password = run_command(f"netsh wlan show profile name=\"{ssid}\" key=clear | findstr Key")
+        password = run_command(f"netsh wlan show profile name=\"{ssid}\" key=clear | findstr Key").replace("\r", "")
         password = re.findall(r"Key Content\s+:\s(.*)", password)[0]
 
     if password == "":
@@ -90,44 +93,26 @@ def generate_qr_code(ssid, password, image=False):
         img.save(file_name)
         print(f"QR code has been saved to {file_name}")
     else:
-        # This will emulate support for ANSI escape sequences, which is needed
-        # in order to display the QR code on Windows
-        colorama.init()
         qr.make()
         qr.print_tty()
 
 
 def main():
-    parser = argparse.ArgumentParser(usage="%(prog)s [options]")
-    parser.add_argument("--qrcode", "-q",
-            action="store_true",
-            default=False,
-            help="Generate a QR code")
-
-    parser.add_argument("--image", "-i",
-            action="store_true",
-            default=False,
-            help="Create the QR code as image instead of showing it on the terminal (must be used along with --qrcode)")
-
-    parser.add_argument("--ssid", "-s",
-            help="Specify a SSID that you have previously connected to")
-
-    parser.add_argument("--version",
-            action="store_true",
-            help="Show version number")
-
+    parser = argparse.ArgumentParser(usage='%(prog)s [options]')
+    parser.add_argument('--qrcode', "-q", action="store_true", default=False, help="Generate a QR code")
+    parser.add_argument('--image', "-i", action="store_true", default=False, help="Create the QR code as image instead of showing it on the terminal (must be used along with --qrcode)")
+    parser.add_argument('--ssid', "-s", default=get_ssid(), help="Specify a SSID that you have previously connected to")
+    parser.add_argument('--version', action="store_true", help="Show version number")
     args = parser.parse_args()
 
     if args.version:
         print(__version__)
         sys.exit()
 
-    if args.ssid is None:
-        args.ssid = get_ssid()
-
     password = get_password(args.ssid)
 
     if args.qrcode:
+        args.no_password = True
         generate_qr_code(args.ssid, password, image=args.image)
         return
 
